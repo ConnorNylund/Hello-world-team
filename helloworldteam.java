5cbc8cec191ffb791372055ff51d2bc01d4a5274
JeremyGerm <jeremyrwiening@gmail.com>
2024-02-14 19:34:19 -0500

    goodnight

--- helloworldteam.java
+++ helloworldteam.java
@@ -2,5 +2,6 @@ public class helloworldteam {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!");
 		System.out.println("Good Evening!");
+		System.out.println("Good Night!");
 	}
 }
